6664ec9dc7a63863f3b3010416f8e1684ce1471a
Luke Bennett <luke.bennett88@gmail.com>
2018-10-23 18:33:35 +1100

    Style carousel

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,23 @@
 const mySiema = new Siema({
   duration: 500,
   loop: true,
+  perPage: {
+    768: 2,
+    992: 3,
+  },
 });
 
 // listen for keydown event
 setInterval(() => mySiema.next(), 4000);
+
+// listen for keydown event
+document.addEventListener('keydown', e => {
+  // if it's left arrow key
+  if (e.keyCode === 37) {
+    mySiema.prev();
+  }
+  // if it's right arrow key
+  else if (e.keyCode === 39) {
+    mySiema.next();
+  }
+});
